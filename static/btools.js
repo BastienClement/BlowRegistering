0e7d6e7927a65170bacfa4ec6731bf51a22db049
galedric <g@ledric.me>
2013-07-05 07:59:33 +0200

    Prevent sending announces for old events

--- static/btools.js
+++ static/btools.js
@@ -968,6 +968,8 @@ var EventViewer = BlowTools.controller("EventViewer", function($scope) {
 			
 		var e = $scope.getEvent();
 		var title = "<b>" + e.title + "</b> à <b>" + e.date.match(/\d{2}:\d{2}/)[0] + "</b>";
+		
+		if(e.state && which != "event-new") return false;
 			
 		switch(which) {
 			case "event-new":
